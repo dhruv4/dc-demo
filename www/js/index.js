50fdb7f9383730ab6043171f6df8a417822f587c
Dhruv Gupta <dhruv4@gmail.com>
2015-07-15 16:33:27 -0400

    initial commit

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+function enterTest(){
+
+	$('#splash-container').remove();
+	$('#test-container').show();
+	$('#demo-container').remove();
+
+}
+function enterDemo(){
+
+	$('#splash-container').remove();
+	$('#test-container').remove();
+	$('#demo-container').show();
+}
