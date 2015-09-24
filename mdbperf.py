4fd91fa508bdddb12726eedb0ec39193e89cc77b
Dhruv <dhruv4@gmail.com>
2015-09-23 23:16:47 -0400

    flush

--- mdbperf.py
+++ mdbperf.py
@@ -116,7 +116,7 @@ def createDCTableLevel1(table, levels, numChunks, numCols, numRows):
 
 			p = findPercent(nodeCount, sizeDC)
 			if(p - prevPercent >= 5):
-				print(str(random.randint(23,28123)) + "|" + str(p) + "&", flush=True, sep="")
+				print(str(random.randint(23,28123)) + "|" + str(p) + "&", sep="")
 				prevPercent = p
 				sys.stdout.flush()
 
@@ -149,7 +149,7 @@ def createDCTableLevel2(table, levels, numChunks, numCols, numRows, nodeCount):
 
 				p = findPercent(nodeCount, sizeDC)
 				if(p - prevPercent >= 5):
-					print(str(random.randint(23,28123)) + "|" + str(p) + "&", flush=True, sep="")
+					print(str(random.randint(23,28123)) + "|" + str(p) + "&", sep="")
 					prevPercent = p
 					sys.stdout.flush()
 
@@ -190,7 +190,7 @@ def createDCTableLeveln(table, levels, numChunks, numCols, numRows, nodeCount):
 
 			p = findPercent(nodeCount, sizeDC)
 			if(p - prevPercent >= 5):
-				print(str(random.randint(23,28123)) + "|" + str(p) + "&", flush=True, sep="")
+				print(str(random.randint(23,28123)) + "|" + str(p) + "&", sep="")
 				prevPercent = p
 				sys.stdout.flush()
 
