b8bbebc30043a6b6e343e302463a8131d17eae9c
Dhruv <dhruv4@gmail.com>
2015-09-23 23:23:46 -0400

    messing with clock

--- www/js/indexc.js
+++ www/js/indexc.js
@@ -151,7 +151,7 @@ socket.on('pgNews', function (msg){
 
 	$('#pg-prog').css('width', String(msg['percent']) + "%");
 	
-	chart.options.data[2].dataPoints.push({ x: parseInt(msg['percent']), y: totalSeconds});
+	chart.options.data[2].dataPoints.push({ x: parseInt(msg['percent']), y: pgtimer.clock/1000});
 	chart.render();
 
 	pgCache+=parseInt(msg['cache']);
