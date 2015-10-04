7c18c153dd4dc4a02a3373716006a67aff8a7c65
Dhruv Gupta <dhruv4@gmail.com>
2015-10-04 01:25:09 -0400

    experiment with starting/stopping monet/postgres from app.js

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-
-    postgres = sudo(['-u', 'postgres', '/usr/lib/postgresql/9.1/bin/pg_ctl', '-D', '/var/lib/postgresql/9.1/main/', 'start']);
+    exec('sudo -u postgres /usr/lib/postgresql/9.1/bin/pg_ctl -D /var/lib/postgresql/9.1/main/ start');
+    //postgres = sudo(['-u', 'postgres', '/usr/lib/postgresql/9.1/bin/pg_ctl', '-D', '/var/lib/postgresql/9.1/main/', 'start']);
     monet = spawn('/usr/local/bin/mserver5', ['--dbpath=/home/gupta/mydbfarm/test', '--set', 'merovingian_uri=mapi:monetdb://adama:50000/test', '--set', 'mapi_open=false', '--set', 'mapi_port=50000', '--set', 'mapi_usock=/home/gupta/mydbfarm/test/.mapi.sock', '--set', 'monet_vault_key=/home/gupta/mydbfarm/test/.vaultkey', '--set', 'gdk_nr_threads=1', '--set', 'max_clients=64', '--set', 'sql_optimizer=default_pipe', '--set', 'monet_daemon=yes']);
 
     var pgPerf, mdbPerf, cPerf;
